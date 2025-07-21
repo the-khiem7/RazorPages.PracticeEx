aff981adc597603487109773b8ef5c66346e5c5a
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-07-21 08:47:30 +0700

    add Account cshtml Folder

---
+++ Account/Forbidden.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LionPetManagement_DoanNgocTrung.Pages.Account
+{
+    public class ForbiddenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
